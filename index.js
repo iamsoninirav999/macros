eee201bb83ff7cd93291aae19c0374c4e37404ae
iamsoninirav999 <158059921+iamsoninirav999@users.noreply.github.com>
2024-01-30 06:14:46 +0000

    done

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ console.log("Feature B added ");
 
 const age = 10; 
 console.log(age);
+console.log("Created index file");
+
+console.log("Feature B added ");
+
+
+console.log(age);
+console.log("Created index file");
+
+console.log("Feature B added ");
+
+console.log(age);
