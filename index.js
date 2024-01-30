5a8b7943d1e6a954d8e650454ee9336df66a5f0e
Soni Nirav <soninirav1908@gmail.com>
2024-01-30 11:56:43 +0530

    loop '

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-console.log("Created index file");
+console.assert("hel s");
 
-console.log("Feature B added ");
-
-const newAge = 10;
-console.log(newAge);
-const age = 10;
-
-console.log(age);
-console.log("Created index file");
-
-console.log("Feature B added ");
-
-console.log(age);
-console.log("Created index file");
-
-console.log("Feature B added ");
-
-console.log(age);
-// const age = 10;
-console.log(age);
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
